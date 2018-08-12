f40d57b3eb6711aa7b4851deea89de2da49c3ece
Igor <qgosha@list.ru>
2018-08-12 14:25:13 -0600

    deploypre

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const mongoser = 'mongodb://admin:416342wQ@ds119662.mlab.com:19662/appdb';
+const mongoser = 'mongodb://admin:416342wQ@ds119662.mlab.com:19662/appdb';
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoser || 'mongodb://localhost:27017/TodoApp');
 
